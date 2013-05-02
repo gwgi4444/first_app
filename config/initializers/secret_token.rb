c583fe5594af5acdf8fbb36584fa3962686a237c
George <webmaster@bondagebeacon.com>
2013-05-02 16:56:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2c039e8a555ab7bf2f02aa1ca8b4aa6de67a74e260609a74ee7d4c41a199922cc5731d3840511d16a31ccfad70f5cb1fb8f7a7c17c8a1173657bef689a2d5912'
